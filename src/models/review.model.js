6527aef67ef2b7025fea2369865899a92d9c4cd2
dev-ahad-ali <ahadali.dev@gmail.com>
2026-01-12 20:10:08 +0600

    Update: user, genre, book, review model added

---
+++ src/models/review.model.js
@@ -0,0 +1,36 @@
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+
+    book: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Book',
+      required: true,
+    },
+
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+
+    comment: {
+      type: String,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: ['pending', 'approved'],
+      default: 'pending',
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('Review', reviewSchema);
