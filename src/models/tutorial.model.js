2f83ec3f08600a870be5761d8a74a848a876eff4
dev-ahad-ali <ahadali.dev@gmail.com>
2026-01-12 20:12:13 +0600

    Update: tutorial model added

---
+++ src/models/tutorial.model.js
@@ -0,0 +1,23 @@
+const tutorialSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    youtubeUrl: {
+      type: String,
+      required: true,
+    },
+
+    description: String,
+
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('Tutorial', tutorialSchema);
