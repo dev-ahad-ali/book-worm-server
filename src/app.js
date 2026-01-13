eab5fd18d1a0206137e2645ebfce6bdd6fc66b58
dev-ahad-ali <ahadali.dev@gmail.com>
2026-01-13 23:29:10 +0600

    Update: all routes added to app js file

--- src/app.js
+++ src/app.js
@@ -16,8 +16,16 @@ app.use(cookieParser());
 
 // routes import
 import authRoutes from './routes/auth.routes.js';
+import bookRoutes from './routes/book.routes.js';
+import genreRoutes from './routes/genre.routes.js';
+import reviewRoutes from './routes/review.routes.js';
+import shelfRoutes from './routes/shelf.routes.js';
 
 //routes declaration
 app.use('/api/auth', authRoutes);
+app.use('/api/books', bookRoutes);
+app.use('/api/genres', genreRoutes);
+app.use('/api/reviews', reviewRoutes);
+app.use('/api/shelves', shelfRoutes);
 
 export { app };
