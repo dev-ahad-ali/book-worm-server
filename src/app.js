1deb0fad057a60ca71d554b9f54bb7ffef73a3ee
dev-ahad-ali <ahadali.dev@gmail.com>
2026-01-14 00:21:49 +0600

    Update: registered all the routes

--- src/app.js
+++ src/app.js
@@ -20,6 +20,10 @@ import bookRoutes from './routes/book.routes.js';
 import genreRoutes from './routes/genre.routes.js';
 import reviewRoutes from './routes/review.routes.js';
 import shelfRoutes from './routes/shelf.routes.js';
+import recommendationRoutes from './routes/recommendation.routes.js';
+import adminRoutes from './routes/admin.routes.js';
+import readingStatsRoutes from './routes/readingStats.routes.js';
+import activityRoutes from './routes/activity.routes.js';
 
 //routes declaration
 app.use('/api/auth', authRoutes);
@@ -27,5 +31,9 @@ app.use('/api/books', bookRoutes);
 app.use('/api/genres', genreRoutes);
 app.use('/api/reviews', reviewRoutes);
 app.use('/api/shelves', shelfRoutes);
+app.use('/api/recommendations', recommendationRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/reading-stats', readingStatsRoutes);
+app.use('/api/activity', activityRoutes);
 
 export { app };
